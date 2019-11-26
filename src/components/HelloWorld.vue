dccc6573919ff44367e7d28b46add40164864f03
zbyzbyzby3 <2034898973@qq.com>
2019-11-26 20:59:04 +0800

    vuex

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,36 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    {{ this.$store.state.count }}
+    <p>{{ this.$store.getters.getCount }} </p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <br/>
+    <p>{{count1}}</p>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapState, mapActions } from "vuex" 
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      count: this.$store.state.count
+    }
+  },
+  computed: {
+    ...mapState({
+      count1: state => state.count
+    })
+  },
+  methods: {
+    increment() {
+      this.$store.dispatch('increment')
+      // this.$store.commit('increment')
+    },
+    decrement() {
+      this.$store.dispatch('decrement')
+      // this.$store.commit('decrement')
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,31 @@
-import vue from "vue"
+import Vue from "vue"
 import vuex from "vuex"
 Vue.use(vuex)
+
+const store = new vuex.Store({
+    state: {
+        count: 1
+    },
+    getters: {
+       getCount(state) {
+            return state.count + 1
+       }
+    },
+    mutations: {
+        increment (state) {
+            state.count = state.count + 1
+        },
+        decrement (state) {
+            state.count = state.count - 1
+        }
+    },
+    actions: {
+      increment(context) {
+        context.commit('increment')
+      },
+      decrement(context) {
+        context.commit('decrement')
+      }
+    }
+})
+export default store
